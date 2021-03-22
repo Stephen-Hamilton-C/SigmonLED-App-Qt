aa0c61e8939aba088db33d2299d07aab0f2b9218
Stephen Hamilton <stephen.hamilton.c@gmail.com>
2021-03-22 18:28:14 -0400

    Removed unnecessary tinkering scripts

--- palettecolormodel.cpp
+++
@@ -1,7 +0,0 @@
-#include "palettecolormodel.h"
-
-PaletteColorModel::PaletteColorModel(QString labelText, QColor color, QObject *parent) : QObject(parent)
-{
-	this->labelText = labelText;
-	this->color = color;
-}
